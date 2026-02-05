7311dd2e16f3e77988d54d41f11f1c17d85eee88
Vinayak Varshney <vinayakvarshney18@gmail.com>
2026-02-05 21:38:25 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,5 +12,5 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
-  impersonate_service_account = var.deployment_sa
+  #impersonate_service_account = var.deployment_sa
 }
